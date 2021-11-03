cd7d172e822f289ca64eb02438a0ed3afd20447f
Luk-kar <59205324+Lukkar90@users.noreply.github.com>
2021-11-03 11:00:15 +0100

    update

--- Show_Images_Differences/check_argv_correctness/helpers/check_show_diffrences.py
+++ Show_Images_Differences/check_argv_correctness/helpers/check_show_diffrences.py
@@ -11,9 +11,10 @@ from Show_Images_Differences.utils import check_show_differences_argv
 # same lib
 from Show_Images_Differences.check_argv_correctness.helpers.errors import ERRORS_MESSAGES
 
-def check_show_diffrences(argv_):
+
+def check_show_differences(argv_):
     """Check if "show red rectangles" argument is correct"""
 
-    if not check_show_differences_argv(argv_): 
+    if not check_show_differences_argv(argv_):
         sys.exit(f'{ERRORS_MESSAGES["invalid show differences"]}\n'
-            f"{help_tip()}")
+                 f"{help_tip()}")
